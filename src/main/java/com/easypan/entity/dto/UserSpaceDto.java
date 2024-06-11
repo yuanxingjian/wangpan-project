026e86f650838e3851d7b5157b60c04186ad876b
yuanxingjian <166608284+yuanxingjian@users.noreply.github.com>
2024-06-11 20:10:31 +0800

    Add files via upload

---
+++ src/main/java/com/easypan/entity/dto/UserSpaceDto.java
@@ -0,0 +1,27 @@
+package com.easypan.entity.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserSpaceDto implements Serializable {
+    private Long useSpace;
+    private Long totalSpace;
+
+    public Long getUseSpace() {
+        return useSpace;
+    }
+
+    public void setUseSpace(Long useSpace) {
+        this.useSpace = useSpace;
+    }
+
+    public Long getTotalSpace() {
+        return totalSpace;
+    }
+
+    public void setTotalSpace(Long totalSpace) {
+        this.totalSpace = totalSpace;
+    }
+}
